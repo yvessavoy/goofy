4db72518a99f759f49a1fbb77d5d5f8344f73bff
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2020-11-05 18:15:57 +0100

    Return err for user routes

--- src/users.rs
+++ src/users.rs
@@ -2,20 +2,24 @@ use crate::Client;
 use crate::GoofyError;
 use crate::Profile;
 use crate::API_BASE_URL;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct UsersResponse {
+    user: Profile,
+}
 
 impl Client {
     pub fn get_profile_by_username(&self, username: &str) -> Result<Profile, GoofyError> {
         let url = format!("{}/users/{}/usernameinfo/", API_BASE_URL, username);
-        let resp = self.http.get(&url).send()?;
-        if resp.status() != 200 {
-            return Err(GoofyError::ResponseNotSuccess(resp.status().as_u16()));
+        let r = self.http.get(&url).send()?;
+        if r.status() != 200 {
+            return Err(GoofyError::ResponseNotSuccess(r.status().as_u16()));
         }
 
-        let resp_json = resp.json::<serde_json::Value>()?;
-        let user = resp_json["user"].clone();
-        let profile: Profile = serde_json::from_value(user)?;
+        let response: UsersResponse = r.json()?;
 
-        Ok(profile)
+        Ok(response.user)
     }
 
     pub fn get_profile_by_id(&self, id: i64) -> Result<Profile, GoofyError> {
