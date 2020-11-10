6e5a890d21496b0c85563aacfe9ebbcbaa3a6ceb
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2020-11-10 20:37:56 +0100

    Mock all tests with static JSON responses

--- src/tests.rs
+++ src/tests.rs
@@ -1,45 +1,75 @@
 use crate::Client;
+use mockito::mock;
+use mockito::Mock;
+use std::path::Path;
 
 #[test]
-fn test_get_profile_by_username() {
+fn get_profile_by_username() {
+    let _mock = create_mock(
+        "/users/jakebaecher/usernameinfo/",
+        "user_name_success.json",
+        200,
+    );
+
     let client = get_client();
 
     let profile = client
-        .get_profile_by_username("thesecretyves")
+        .get_profile_by_username("jakebaecher")
         .expect("Error fetching profile");
-    assert_eq!(profile.username, "thesecretyves");
+    assert_eq!(profile.username, "jakebaecher");
 }
 
 #[test]
-fn test_get_profile_by_id() {
+fn get_profile_by_id() {
+    let _mock = create_mock("/users/44487504243/info/", "user_id_success.json", 200);
+
     let client = get_client();
 
     let profile = client
-        .get_profile_by_id(21882022536)
+        .get_profile_by_id(44487504243)
         .expect("Error fetching profile");
-    assert_eq!(profile.username, "thesecretyves");
+    assert_eq!(profile.username, "jakebaecher");
 }
 
 #[test]
-fn test_get_followers() {
+fn get_followers() {
+    let _m = create_mock(
+        "/friendships/44487504243/followers/?max_id=&ig_sig_key_version=4",
+        "get_followers.json",
+        200,
+    );
     let client = get_client();
     let followers = client
-        .get_followers(21882022536)
+        .get_followers(44487504243)
         .expect("Failed to fetch followers");
-    assert!(followers.len() > 0);
+    assert!(followers.len() == 11);
 }
 
 #[test]
-fn test_get_following() {
+fn get_following() {
+    let _m = create_mock(
+        "/friendships/44487504243/following/?max_id=0&ig_sig_key_version=4",
+        "get_following.json",
+        200,
+    );
     let client = get_client();
     let following = client
-        .get_following(21882022536)
+        .get_following(44487504243)
         .expect("Failed to fetch following");
-    assert!(following.len() > 0);
+    assert!(following.len() == 78);
+}
+
+fn create_mock(url: &str, response_path: &str, status: usize) -> Mock {
+    let response = std::fs::read_to_string(format!("test_responses/{}", response_path)).unwrap();
+    mock("GET", url)
+        .with_status(status)
+        .with_header("content-type", "application/json")
+        .with_body(response)
+        .create()
 }
 
 fn get_client() -> Client {
-    if std::path::Path::new("session.txt").exists() {
+    if Path::new("session.txt").exists() {
         Client::import("session.txt").expect("Error importing client")
     } else {
         let username = std::env::var("GOOFY_USER").expect("GOOFY_USER not set");
